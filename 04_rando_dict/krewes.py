6520c595bbf2d9dcecdc63c630374e7a17373b4e
Caden Khuu <117611429+cadenk0@users.noreply.github.com>
2024-09-13 11:48:36 -0400

    Add files via upload

---
+++ 04_rando_dict/krewes.py
@@ -0,0 +1,28 @@
+"""Caden Khuu
+CAD
+SoftDev
+Random Selection of Devos
+2024-09-13
+time spend: """
+
+krewes = {
+       	4: [
+    	'DUA','TAWAB','EVA','JACK','VICTOR','EVAN','JASON','COLYI','IVAN','TANZEEM',
+    	'TAHMIM','STANLEY','LEON','NAOMI','NIA','ANASTASIA','JADY','BRIAN','JACOB',
+    	'ALEX','CHONGTIAN','DANNY','MARCO','ABIDUR','ANKITA','ANDY','ETHAN','AMANDA',
+    	'AIDAN','LINDA','QIANJUN','JIAYING','KISHI'
+    	],
+       	5: [
+            	'ADITYA','MARGIE','RACHEL','ALEXANDER','ZIYAD','DANNY','ENDRIT','CADEN',
+            	'VEDANT','SUHANA','KYLE','KEVIN','RAYMOND','CHRISTOPHER','JONATHAN','SASHA',
+            	'NAFIYU','TIM','WILL','DANIEL','BENJAMIN','CLAIRE','CHLOE','STELLA','TRACY',
+            	'JESSICA','JACKIE','WEN YUAN','YINWEI','TIFFANY','JAYDEN DANIEL','PRINCEDEN'
+          	]
+     	}
+
+def randDevo():
+	pd = randint(4,5)
+    
+
+
+
