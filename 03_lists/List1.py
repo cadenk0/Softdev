e051ba0d11d315c71e21b7ff7632e6652544aa0c
Caden Khuu <117611429+cadenk0@users.noreply.github.com>
2024-09-17 11:23:56 -0400

    Update List1.py

--- 03_lists/List1.py
+++ 03_lists/List1.py
@@ -1,3 +1,8 @@
+'''Given an array of ints, return True if 6 appears as either the first or last element in the array. The array will be length 1 or more.
+
+first_last6([1, 2, 6]) → True
+first_last6([6, 1, 2, 3]) → True
+first_last6([13, 6, 1, 2, 3]) → False'''
 def first_last6(nums):
   if nums[0] == 6:
     return True
@@ -6,8 +11,14 @@ def first_last6(nums):
     
   return False
 
-print(first_last6([1, 2, 3, 4, 5, 6]))
+first_last6([1, 2, 3, 4, 5, 6])
 
+
+'''Given an array of ints, return True if the array is length 1 or more, and the first element and the last element are equal.
+
+same_first_last([1, 2, 3]) → False
+same_first_last([1, 2, 3, 1]) → True
+same_first_last([1, 2, 1]) → True'''
 def same_first_last(nums):
   if len(nums) >= 1:
     if nums[0] == nums[-1]:
@@ -18,23 +29,41 @@ def same_first_last(nums):
     return(False)
 
 
+'''Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+
+make_pi() → [3, 1, 4]'''
 def make_pi():
   a = [3, 1, 4]
   return(a)
 
 
+'''Given 2 arrays of ints, a and b, return True if they have the same first element or they have the same last element. Both arrays will be length 1 or more.
+
+common_end([1, 2, 3], [7, 3]) → True
+common_end([1, 2, 3], [7, 3, 2]) → False
+common_end([1, 2, 3], [1, 3]) → True''''
 def common_end(a, b):
   if a[0]==b[0] or a[-1]==b[-1]:
     return(True)
   else:
     return(False)
 
+'''Given an array of ints length 3, return the sum of all the elements.
+
+sum3([1, 2, 3]) → 6
+sum3([5, 11, 2]) → 18
+sum3([7, 0, 0]) → 7'''
 def sum3(nums):
   sum = 0
   for i in nums:
     sum += i
   return sum
 
+'''Given an array of ints length 3, return an array with the elements "rotated left" so {1, 2, 3} yields {2, 3, 1}.
+
+rotate_left3([1, 2, 3]) → [2, 3, 1]
+rotate_left3([5, 11, 9]) → [11, 9, 5]
+rotate_left3([7, 0, 0]) → [0, 0, 7]'''
 def rotate_left3(nums):
   nums.append(nums.pop(0))
   return nums
@@ -43,8 +72,8 @@ def reverse3(nums):
   count = 2
   result = []
   for i in range(3):
-    result.append(nums.pop(count))
-    count -= 1
+	result.append(nums.pop(count))
+	count -= 1
   return result
 
 '''
@@ -58,7 +87,7 @@ reverse3([7, 0, 0]) → [0, 0, 7]
 def max_end3(nums):
   m = max(nums[0], nums[len(nums)-1])
   for i in range(len(nums)):
-    nums[i-1] = m
+	nums[i-1] = m
   return nums
 
 '''
@@ -82,7 +111,7 @@ sum2([1, 1, 1, 1]) → 2
 
 def middle_way(a, b):
   result = []
-  result.append( a[1]) 
+  result.append( a[1])
   result.append(b[1])
   return result
 
@@ -118,4 +147,3 @@ has23([2, 5]) → True
 has23([4, 3]) → True
 has23([4, 5]) → False
 '''
-
