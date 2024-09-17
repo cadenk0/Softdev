7d5e14bca56db765180d0b1c28df3e6f073b0f92
Caden Khuu <117611429+cadenk0@users.noreply.github.com>
2024-09-17 11:19:54 -0400

    add comments list1

--- 03_lists/List1.py
+++ 03_lists/List1.py
@@ -47,14 +47,38 @@ def reverse3(nums):
     count -= 1
   return result
 
+'''
+Given an array of ints length 3, return a new array with the elements in reverse order, so {1, 2, 3} becomes {3, 2, 1}.
+
+reverse3([1, 2, 3]) → [3, 2, 1]
+reverse3([5, 11, 9]) → [9, 11, 5]
+reverse3([7, 0, 0]) → [0, 0, 7]
+'''
+
 def max_end3(nums):
   m = max(nums[0], nums[len(nums)-1])
   for i in range(len(nums)):
     nums[i-1] = m
   return nums
 
+'''
+Given an array of ints length 3, figure out which is larger, the first or last element in the array, and set all the other elements to be that value. Return the changed array.
+
+max_end3([1, 2, 3]) → [3, 3, 3]
+max_end3([11, 5, 9]) → [11, 11, 11]
+max_end3([2, 11, 3]) → [3, 3, 3]
+'''
+
 def sum2(nums):
   return sum(nums[:2])
+
+'''
+Given an array of ints, return the sum of the first 2 elements in the array. If the array length is less than 2, just sum up the elements that exist, returning 0 if the array is length 0.
+
+sum2([1, 2, 3]) → 3
+sum2([1, 1]) → 2
+sum2([1, 1, 1, 1]) → 2
+'''
 
 def middle_way(a, b):
   result = []
@@ -62,12 +86,36 @@ def middle_way(a, b):
   result.append(b[1])
   return result
 
+'''
+Given 2 int arrays, a and b, each length 3, return a new array length 2 containing their middle elements.
+
+middle_way([1, 2, 3], [4, 5, 6]) → [2, 5]
+middle_way([7, 7, 7], [3, 8, 0]) → [7, 8]
+middle_way([5, 2, 9], [1, 4, 5]) → [2, 4]
+'''
+
 def make_ends(nums):
   result = []
   result.append(nums[0])
   result.append(nums[len(nums)-1])
   return result
 
+'''
+Given an array of ints, return a new array length 2 containing the first and last elements from the original array. The original array will be length 1 or more.
+
+make_ends([1, 2, 3]) → [1, 3]
+make_ends([1, 2, 3, 4]) → [1, 4]
+make_ends([7, 4, 6, 2]) → [7, 2]
+'''
+
 def has23(nums):
   return(2 in nums or 3 in nums)
+
+'''
+Given an int array length 2, return True if it contains a 2 or a 3.
+
+has23([2, 5]) → True
+has23([4, 3]) → True
+has23([4, 5]) → False
+'''
 
