962db4f19f4c443110ab4e57963f957b9c6c5ae6
Caden Khuu <117611429+cadenk0@users.noreply.github.com>
2024-10-16 11:42:30 -0400

    live style

---
+++ 18_live_stuyle/app.py
@@ -0,0 +1,28 @@
+'''
+CAD - Caden, Aditya, Danny
+SoftDev
+K16 - Flask-Sessions - Using Flask and cookies
+2024 - 10 - 09
+Time Spent:  
+'''
+
+# import conventions:
+# list most general first (standard python library)
+# ...then pip installs (eg Flask)
+# ...then your own home-rolled modules/packages (today's test module)
+
+from flask import Flask             #facilitate flask webserving
+from flask import render_template   #facilitate jinja templating
+from flask import request           #facilitate form submission
+from flask import session
+
+app = Flask(__name__)    #create Flask object
+
+@app.route("/")
+def disp():
+    return render_template('templates/index.html')
+
+if __name__ == "__main__": #false if this file imported as module
+    #enable debugging, auto-restarting of server when this file is modified
+    app.debug = True 
+    app.run()
